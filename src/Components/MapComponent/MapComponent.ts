f173673861cd63a69355d5de9a43ee773f1bd99c
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2026-02-03 15:12:33 +0700

    Добавлена заготовка для компонента карты

--- src/Components/MapComponent/MapComponent.ts
+++ src/Components/MapComponent/MapComponent.ts
@@ -1,1 +1,25 @@
-export default class MapComponent {}
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { Map } from "maptalks-gl";
+
+@Component({
+  selector: "MapComponent",
+  templateUrl: "MapComponent.html",
+  styleUrls: ["./MapComponent.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class MapComponent implements OnInit {
+  @ViewChild("Container", { static: true })
+  Container!: ElementRef<HTMLDivElement>;
+  Map!: Map;
+  /*Инициализация карты*/
+  MapInit() {}
+  ngOnInit(): void {
+    this.MapInit();
+  }
+}
